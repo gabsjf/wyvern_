af00882bb672ba06b0ff127d71a8ae3b4a66bf69
Gabriel Felix <gabrieljf268@gmail.com>
2026-07-16 10:37:36 -0400

    Add LegacyTimestamp migration

--- Wyvern/Wyvern.Infrastructure/Migrations/20260716143725_LegacyTimestamp.Designer.cs
+++ Wyvern/Wyvern.Infrastructure/Migrations/20260716143725_LegacyTimestamp.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Wyvern.Infrastructure.Data;
@@ -11,9 +12,11 @@ using Wyvern.Infrastructure.Data;
 namespace Wyvern.Infrastructure.Migrations
 {
     [DbContext(typeof(WyvernDbContext))]
-    partial class WyvernDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260716143725_LegacyTimestamp")]
+    partial class LegacyTimestamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
