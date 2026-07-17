28458e4f8f68f78c72c5272929acf73e3795fcf0
Gabriel Felix <gabrieljf268@gmail.com>
2026-07-17 15:50:15 -0400

    Configure Android Deep Links

--- Wyvern.Front/src/app/app.ts
+++ Wyvern.Front/src/app/app.ts
@@ -1,16 +1,29 @@
-import { Component, signal, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 import { Sidebar } from './core/layout/sidebar/sidebar';
 import { AuthService } from './core/services/auth.service';
 import { CommonModule } from '@angular/common';
+import { App as CapacitorApp } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, CommonModule],
+  imports: [CommonModule, RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('wyvern-front');
   authService = inject(AuthService);
+  router = inject(Router);
+
+  ngOnInit() {
+    // Escuta eventos de Deep Link
+    CapacitorApp.addListener('appUrlOpen', (event: any) => {
+      const slug = event.url.split('.app').pop();
+      if (slug) {
+        // Redireciona o Angular para a rota capturada do link
+        this.router.navigateByUrl(slug);
+      }
+    });
+  }
 }
