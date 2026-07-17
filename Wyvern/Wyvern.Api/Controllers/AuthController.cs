d7a22cffe7955dcb0e7a96cf222af9b479af4aac
Gabriel Felix <gabrieljf268@gmail.com>
2026-07-17 15:19:51 -0400

    Add fix-db endpoint

--- Wyvern/Wyvern.Api/Controllers/AuthController.cs
+++ Wyvern/Wyvern.Api/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Wyvern.Application.DTOs.Auth;
 using Wyvern.Application.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Wyvern.Api.Controllers
 {
@@ -10,10 +11,26 @@ namespace Wyvern.Api.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly Wyvern.Infrastructure.Data.WyvernDbContext _db;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService, Wyvern.Infrastructure.Data.WyvernDbContext db)
         {
             _authService = authService;
+            _db = db;
+        }
+
+        [HttpGet("fix-db")]
+        public IActionResult FixDb()
+        {
+            try
+            {
+                _db.Database.ExecuteSqlRaw("ALTER TABLE \"Usuarios\" ADD COLUMN IF NOT EXISTS \"Papel\" text NOT NULL DEFAULT 'Jogador';");
+                return Ok("Fixed");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
         }
 
         [HttpPost("register")]
